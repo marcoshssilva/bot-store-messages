a6543fdbcfb8e1b4bffacd86118dd706eda41eb5
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-06 22:27:38 -0300

    initial project

---
+++ src/main/java/br/com/marcoshssilva/botstoremessages/BotStoreMessagesApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.botstoremessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotStoreMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotStoreMessagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/marcoshssilva/botstoremessages/BotStoreMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcoshssilva.botstoremessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotStoreMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
