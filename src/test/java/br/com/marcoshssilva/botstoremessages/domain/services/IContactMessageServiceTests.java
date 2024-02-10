894649005eac14c9ac894d2c3e893debf585a3d8
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-10 18:49:34 -0300

    tests: make tests to IContactMessageServiceTests using mockito

--- src/test/java/br/com/marcoshssilva/botstoremessages/domain/services/IContactMessageServiceTests.java
+++ src/test/java/br/com/marcoshssilva/botstoremessages/domain/services/IContactMessageServiceTests.java
@@ -10,20 +10,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @DataMongoTest
 @ExtendWith(SpringExtension.class)
 class IContactMessageServiceTests {
+
+    @InjectMocks
     private ContactMessageServiceImpl contactMessageService;
 
-    @Autowired
+    @Mock
     private ContactMessageRepository repository;
 
     @BeforeEach
@@ -36,24 +46,32 @@ class IContactMessageServiceTests {
     void testRegisterNewContactMessage_Success() throws ContactMessageCannotBeCreatedException {
         // Mocking input data
         ContactMessageNew contactMessageNew = new ContactMessageNew("John", "john@example.com", "Hello");
+        ContactMessage contactMessage = ContactMessage.builder().id("1212121212").name("John").email("john@example.com").message("Hello").build();
 
+        // Mock methods
+        when(repository.save(any())).thenReturn(contactMessage);
         // Testing service method
         Optional<ContactMessageData> result = contactMessageService.registerNewContactMessage(contactMessageNew);
 
         // Assertions
         assertTrue(result.isPresent());
-        assertNotEquals(null, result.get().id());
-
+        assertEquals("1212121212", result.get().id());
         assertEquals("John", result.get().name());
         assertEquals("john@example.com", result.get().email());
         assertEquals("Hello", result.get().message());
 
-        final Optional<ContactMessage> message = repository.findById(result.get().id());
-
-        assertTrue(message.isPresent());
-        assertEquals("John", message.get().getName());
-        assertEquals("john@example.com", message.get().getEmail());
-        assertEquals("Hello", message.get().getMessage());
+        // check that repository save must be called
+        verify(repository, times(1)).save(any());
     }
 
+    @DisplayName("must throw a ContactMessageCannotBeCreatedException when try save in database and gets error")
+    @Test
+    void testRegisterNewContactMessage_FailedBecause_RepositoryFails() {
+        // Mocking input data
+        ContactMessageNew contactMessageNew = new ContactMessageNew("John", "john@example.com", "Hello");
+        // Mock methods
+        when(repository.save(any())).thenThrow(new DataIntegrityViolationException("Database is OFFLINE"));
+        // Testing service method
+        assertThrows(ContactMessageCannotBeCreatedException.class, () -> contactMessageService.registerNewContactMessage(contactMessageNew));
+    }
 }
