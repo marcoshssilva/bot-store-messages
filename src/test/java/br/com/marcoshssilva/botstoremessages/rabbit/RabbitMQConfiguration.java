df6334b241d2429b47c30e772c977c9667f2628c
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-10 19:02:41 -0300

    feat: add default RabbitMQConfiguration

---
+++ src/test/java/br/com/marcoshssilva/botstoremessages/rabbit/RabbitMQConfiguration.java
@@ -0,0 +1,38 @@
+package br.com.marcoshssilva.botstoremessages.rabbit;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    /**
+     * Get instance of AmqpTemplate to perform operations on AMQP Server as send/receive messages by
+     * queues or channels
+     *
+     * @param connectionFactory FactoryBuilder that instance connection with AMQP Server
+     * @return org.springframework.amqp.core.RabbitTemplate.class
+     */
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        // config to send any payload as Json, when producer mode
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
+
+    /**
+     * Get instance to convert payload received by queue as JSON
+     * @return org.springframework.amqp.support.converter.Jackson2JsonMessageConverter.class
+     */
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
