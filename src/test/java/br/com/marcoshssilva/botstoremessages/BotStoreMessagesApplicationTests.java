cb86ced4fc129b00fc7c3d71a15500d3c6c3b371
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-17 22:22:11 -0300

    refactor: fixed code smell need at least one assertion

--- src/test/java/br/com/marcoshssilva/botstoremessages/BotStoreMessagesApplicationTests.java
+++ src/test/java/br/com/marcoshssilva/botstoremessages/BotStoreMessagesApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.marcoshssilva.botstoremessages;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@lombok.extern.slf4j.Slf4j
 @SpringBootTest
 class BotStoreMessagesApplicationTests {
 
+	@DisplayName("must initialize project with Success")
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> log.info("Project started with success!"));
 	}
 
 }
