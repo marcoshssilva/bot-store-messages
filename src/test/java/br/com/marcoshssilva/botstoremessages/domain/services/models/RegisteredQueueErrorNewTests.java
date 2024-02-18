a37056e9733a2a60e6d21a3c9c5b76d96bf2aafc
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-17 23:13:38 -0300

    tests: coverage record RegisteredQueueErrorNew

---
+++ src/test/java/br/com/marcoshssilva/botstoremessages/domain/services/models/RegisteredQueueErrorNewTests.java
@@ -0,0 +1,35 @@
+package br.com.marcoshssilva.botstoremessages.domain.services.models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class RegisteredQueueErrorNewTests {
+
+    @DisplayName("must validate equals() and hashCode() methods")
+    @Test
+    void testEqualsAndHashCode() {
+        RegisteredQueueErrorNew error1 = new RegisteredQueueErrorNew("Error 1", "data1".getBytes());
+        RegisteredQueueErrorNew error2 = new RegisteredQueueErrorNew("Error 1", "data1".getBytes());
+        RegisteredQueueErrorNew error3 = new RegisteredQueueErrorNew("Error 2", "data2".getBytes());
+
+        // Test equals method
+        assertEquals(error1, error2);
+        assertNotEquals(error1, error3);
+
+        // Test hashCode method
+        assertEquals(error1.hashCode(), error2.hashCode());
+        assertNotEquals(error1.hashCode(), error3.hashCode());
+    }
+
+    @DisplayName("must call toString() with expected string")
+    @Test
+    void testToString() {
+        RegisteredQueueErrorNew error = new RegisteredQueueErrorNew("Error message", "Some data".getBytes());
+        String expectedToString = "RegisteredQueueErrorNew(errorMessage=Error message, errorData=[83, 111, 109, 101, 32, 100, 97, 116, 97])";
+
+        assertEquals(expectedToString, error.toString());
+    }
+}
