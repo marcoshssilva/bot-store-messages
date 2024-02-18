81e134afee9b16c5179bbcc57fc340047d0770e0
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-17 22:43:37 -0300

    refactor: implements hashCode(), equals() and toString() on record RegisteredQueueErrorNew

--- src/main/java/br/com/marcoshssilva/botstoremessages/domain/services/models/RegisteredQueueErrorNew.java
+++ src/main/java/br/com/marcoshssilva/botstoremessages/domain/services/models/RegisteredQueueErrorNew.java
@@ -1,4 +1,26 @@
 package br.com.marcoshssilva.botstoremessages.domain.services.models;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public record RegisteredQueueErrorNew(String errorMessage, byte[] errorData) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RegisteredQueueErrorNew that = (RegisteredQueueErrorNew) o;
+        return Objects.equals(errorMessage, that.errorMessage) && Arrays.equals(errorData, that.errorData);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(errorMessage);
+        result = 31 * result + Arrays.hashCode(errorData);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisteredQueueErrorNew(" + "errorMessage=" + errorMessage + ", errorData=" + Arrays.toString(errorData) + ')';
+    }
 }
