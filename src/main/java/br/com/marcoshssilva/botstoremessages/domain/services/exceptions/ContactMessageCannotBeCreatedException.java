829513a17a0042e59510573cd6a4b320237fba5a
marcoshssilva <76234459+marcoshssilva@users.noreply.github.com>
2024-02-17 22:24:57 -0300

    refactor: removed unused constructor

--- src/main/java/br/com/marcoshssilva/botstoremessages/domain/services/exceptions/ContactMessageCannotBeCreatedException.java
+++ src/main/java/br/com/marcoshssilva/botstoremessages/domain/services/exceptions/ContactMessageCannotBeCreatedException.java
@@ -1,9 +1,6 @@
 package br.com.marcoshssilva.botstoremessages.domain.services.exceptions;
 
 public class ContactMessageCannotBeCreatedException extends Exception {
-    public ContactMessageCannotBeCreatedException(String message) {
-        super(message);
-    }
     public ContactMessageCannotBeCreatedException(String message, Throwable t) {
         super(message, t);
     }
